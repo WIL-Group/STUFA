cac0fc1018533b2ae1799b901fe5257cdfdc963b
Catalogo81 <peacematjato@gmail.com@gmail.com>
2021-01-18 16:42:05 +0200

    now the user can read edit and update their Profile details

--- app/src/main/java/com/example/stufa/activities/StudentProfile.java
+++ app/src/main/java/com/example/stufa/activities/StudentProfile.java
@@ -7,22 +7,24 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.stufa.R;
 import com.example.stufa.app_utilities.QueryAdapter;
 import com.example.stufa.app_utilities.Utilities;
+import com.example.stufa.data_models.Announcement;
+import com.example.stufa.data_models.Booking;
 import com.example.stufa.data_models.Form;
 import com.example.stufa.data_models.Query;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -35,30 +37,32 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StudentProfile extends AppCompatActivity /*implements QueryAdapter.ItemClicked*/ {
 
-    TextView tvStudentNumber, tvFullName, tvNameAndSurname, tvBookingType, tvDateCreated,
-             tvDateRequested, tvRequestType, tvDateOfRequest, tvIsRequestAnswered ;
+    TextView tvStudentNumber, tvStudentNum, tvFullName, tvNameAndSurname, tvBookingType, tvDateCreated,
+            tvDateRequested, tvRequestType, tvDateOfRequest, tvIsRequestAnswered;
     EditText etEmailAddress, etCourse, etCampus;
-    Button btnUpdateDetails, btnDeleteBooking, btnEditBooking , btnEditRequest;
+    Button btnUpdateDetails, btnDeleteBooking, btnEditBooking, btnEditRequest;
 
     FirebaseAuth firebaseAuth;
     FirebaseFirestore firestore;
-    DatabaseReference formRer;
+    FirebaseUser user;
+    DatabaseReference userRer;
 
     Form form;
-    String userID, name, surname, studentNumber, email, course, campus;
+    String userID, name, surname, studentNumber, email, course, campus, bType, date;
 
     RecyclerView recyclerView;
     private QueryAdapter myAdapter;
     RecyclerView.LayoutManager layoutManager;
     private ArrayList<Query> queries;
     Query query;
-    String type, message,qId;
-    DatabaseReference queryReff,submittedQueryReff,databaseReference;
+    String type, message, qId;
+    DatabaseReference queryReff, submittedQueryReff, databaseReference;
     com.google.firebase.database.Query query1;
-    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +71,7 @@ public class StudentProfile extends AppCompatActivity /*implements QueryAdapter.
         setContentView(R.layout.activity_student_profile);
 
         tvStudentNumber = findViewById(R.id.tvStudentNumber);
+        tvStudentNum = findViewById(R.id.tvStudentNum);
         tvFullName = findViewById(R.id.tvFullName);
         tvNameAndSurname = findViewById(R.id.tvNameAndSurname);
         tvBookingType = findViewById(R.id.tvBookingType);
@@ -85,10 +90,11 @@ public class StudentProfile extends AppCompatActivity /*implements QueryAdapter.
 
         firebaseAuth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
+        user = firebaseAuth.getCurrentUser();
         form = new Form();
 
         userID = firebaseAuth.getCurrentUser().getUid();
-        formRer = FirebaseDatabase.getInstance().getReference().child("Profiles");
+        userRer = FirebaseDatabase.getInstance().getReference().child("users");
 
         recyclerView = findViewById(R.id.rvList);
         recyclerView.setHasFixedSize(true);
@@ -96,11 +102,6 @@ public class StudentProfile extends AppCompatActivity /*implements QueryAdapter.
         layoutManager = new LinearLayoutManager(StudentProfile.this);
         recyclerView.setLayoutManager(layoutManager);
 
-        //sets the query list to my adapter
-//        readData(list -> {
-//            myAdapter = new QueryAdapter(list,StudentProfile.this);
-//            recyclerView.setAdapter(myAdapter);
-//        });
 
         //Reads the data entered when the user registered just to check if we can read back the data
         DocumentReference documentReference = firestore.collection("users").document(userID);
@@ -109,22 +110,37 @@ public class StudentProfile extends AppCompatActivity /*implements QueryAdapter.
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
 
                 assert value != null;
+                /*-----------------For the profile details tab----------------*/
                 studentNumber = value.getString("studentNumber");
                 name = value.getString("name");
                 surname = value.getString("surname");
                 email = value.getString("email");
-                //course = value.getString("course");
+                course = value.getString("course");
                 campus = value.getString("campus");
+
 
                 tvStudentNumber.setText(studentNumber);
                 tvFullName.setText(String.format("%s %s", name, surname));
                 etEmailAddress.setText(email);
                 etCampus.setText(campus);
-                //etCourse.setText(course);
+                etCourse.setText(course);
+
+                /*-----------------For the booking details tab----------------*/
+                bType = value.getString("bType");
+                date = value.getString("date");
+
+                tvNameAndSurname.setText(String.format("%s %s", name, surname));
+                tvBookingType.setText(bType);
+                tvDateCreated.setText(date);
+                tvStudentNum.setText(studentNumber);
             }
         });
 
+
+
         btnUpdateDetails.setOnClickListener(v -> {
+
+            updateDetails();
 
         });
 
@@ -142,40 +158,75 @@ public class StudentProfile extends AppCompatActivity /*implements QueryAdapter.
 
     }
 
+    public void updateDetails() {
 
-//    private void readData(CreateQuery.FireBaseCallBack fireBaseCallBack)
+        if(etEmailAddress.getText().toString().isEmpty() || etCampus.getText().toString().isEmpty()
+           /*|| etCourse.getText().toString().isEmpty()*/)
+        {
+            Utilities.show(StudentProfile.this, "One or more fields are empty!");
+            return;
+        }
+
+        String newEmail = etEmailAddress.getText().toString().trim();
+        String newCampus = etCampus.getText().toString().trim();
+        String newCourse = etCourse.getText().toString().trim();
+
+        user.updateEmail(newEmail).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                DocumentReference docRef = firestore.collection("users").document(user.getUid());
+                Map<String,Object> edited = new HashMap<>();
+                edited.put("email", newEmail);
+                edited.put("campus", newCampus);
+                edited.put("course", newCourse);
+
+                docRef.update(edited).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //Utilities.show(StudentProfile.this, "Campus and Course are updated!");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Utilities.show(StudentProfile.this, "Error: " + e.getMessage());
+                    }
+                });
+
+                Utilities.show(StudentProfile.this, "Profile updated!");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+
+                Utilities.show(StudentProfile.this, "Error: " + e.getMessage());
+            }
+        });
+
+    }
+
+//    public void readBooking()
 //    {
-//        queryReff = Utilities.getDatabaseRefence().child("submitted_queries");
-//
-//        queryReff.addValueEventListener(new ValueEventListener() {
+//        DocumentReference docRef = firestore.collection("users").document(user.getUid());
+        //bookingDatabaseRef = FirebaseDatabase.getInstance().getReference().child("bookings");
+
+        //bookingDatabaseRef.child("bookings").addValueEventListener(new ValueEventListener() {
 //            @Override
 //            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                //queries.clear();
-//
-//                for(DataSnapshot ds : snapshot.getChildren())
-//                {
-//                    query = ds.getValue(Query.class);
-//                    //queries.add(query);
-//                }
-//                fireBaseCallBack.onCallBack(queries);
+//                String bookingType = snapshot.getValue();
+//                tvBookingType.setText(bookingType);
 //            }
 //
 //            @Override
 //            public void onCancelled(@NonNull DatabaseError error) {
-//                Toast.makeText(StudentProfile.this, "Error!" + error, Toast.LENGTH_SHORT).show();
 //
 //            }
 //        });
 //    }
-//
-//    @Override
-//    public void onItemClicked(int index) {
-//        query = queries.get(index);
-//    }
-//
-//    private interface FireBaseCallBack
+
+//    public void deleteBooking()
 //    {
-//        void onCallBack(ArrayList<Query> list);
+//
 //    }
 
 }
